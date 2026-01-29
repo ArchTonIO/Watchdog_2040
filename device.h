407e776c54bffb33575a995b1743e6617c492d3b
Antonio Del Cogliano <delchi1199@gmail.com>
2026-01-29 10:43:26 +0100

    Update firmware and hardware version in device.h

--- device.h
+++ device.h
@@ -5,8 +5,8 @@
 #define DEVICE_H
 
 #define HOSTNAME "WD_2040"
-#define FIRMWARE_VERSION "leakybob [1.26.0]"
-#define HARDWARE_VERSION "V 1.0 REV 3"
+#define FIRMWARE_VERSION "leakybob [1.33.0]"
+#define HARDWARE_VERSION "V 1.0 REV3"
 #define DEVICE_NAME "Watchdog_2040"
 #define DEVICE_DISPLAY_NAME "WATCHDOG_2040"
 
